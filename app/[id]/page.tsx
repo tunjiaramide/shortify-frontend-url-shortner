eee8d4a75c206a6a99e589fc7a39a8c808d3179f
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-03-16 06:34:50 +0100

    removed redirect handling from frontend

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -7,32 +7,22 @@ import ErrorMessage from "../Components/ErrorMessage";
 export default function Page() {
   const params = useParams<{ id: string }>();
   const [error, setError] = useState<string | null>(null);
-  const [isRedirected, setIsRedirected] = useState(false); 
-
+  
   useEffect(() => {
     if (!params.id || params.id.length !== 8) {
       setError("URL does not exist");
       return;
     }
-
     const fetchUrlAndRedirect = async () => {
       try {
-        const response = await httpService.get(`/${params.id}`);
-        const originalUrl = response.data.originalUrl;
-
-        if (originalUrl && !isRedirected) {
-          setIsRedirected(true); 
-          window.location.replace(originalUrl);
-        }
+        window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/${params.id}`;
       } catch (err) {
         console.log("Fetch error", err);
         setError( "An error occurred");
       }
     };
-
     fetchUrlAndRedirect();
-  }, [params.id, isRedirected]);
+  }, [params.id]);
 
   if (error) return <ErrorMessage message={error} />;
-  return null; // Nothing renders if redirected
 }
